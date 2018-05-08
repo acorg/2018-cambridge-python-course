28041d5f54b1865bc61b0cd306ff35e2f8916dc1
svdvliet <s.vandervliet@erasmusmc.nl>
2018-05-08 10:11:29 +0200

    Major changes to code
    *Integrate Terry's suggestion of using a `dictionary`
    * Integrate Terry's suggestions of using the `get`' function 
    * Added code to allow changes of starting dilution 
    * Added argparse to allow adding xlsx file and changing the starting dilution from `stdin`
    * Modified saving of converted file to add _py after name

--- day-6/stefan-excel.py
+++ day-6/stefan-excel.py
@@ -1,45 +1,69 @@
 #!/usr/bin/env python
 
-# You'll need to pip import openpyxl
+# You'll need to pip import openpyxl 
 import openpyxl
+import argparse
 
-excelFile = openpyxl.load_workbook('stefan-data.xlsx')
+parser = argparse.ArgumentParser( 
+    description='Script to convert HI data from lab readout to titers.',
+    usage='python3  stefan-excel.py -e [table.xlsx]')
 
-sheet1 = excelFile.active
+parser.add_argument(
+    '-e', action="store", default= None, required=True,
+    help='location of Excel file to parse')
+
+parser.add_argument(
+    '-s', type=int, default=20,
+    help='The starting dillution, default=20.')
+
+
+
+
+args = parser.parse_args()
+std = args.starting_dillution
+stdh = std * 1.5
+co = round(std // 2)
+cutoff = '<%s' % co
+
+table = openpyxl.load_workbook(args.Excelfile)
+
+sheet1 = table.active
+
+convert = {
+    '0' : cutoff,
+    '0.5' : co,
+    '1' : std,
+    '1.5' : stdh,
+    '2' : std*(2**1),
+    '2.5' : stdh*(2**1),
+    '3' : std*(2**2),
+    '3.5' : stdh*(2**2),
+    '4' : std*(2**3),
+    '4.5' : stdh*(2**3),
+    '5' : std*(2**4),
+    '5.5' : stdh*(2**4),
+    '6' : std*(2**5),
+    '6.5' : stdh*(2**5),
+    '7' : std*(2**6),
+    '7.5' : stdh*(2**6),
+    '8' : std*(2**7),
+    '8.5' : stdh*(2**7),
+    '9' : std*(2**8),
+    '9.5' : stdh*(2**8),
+    '10' : std*(2**9),
+    '10.5' : stdh*(2**9),
+    '11' : std*(2**10),
+    '11.5' : stdh*(2**10),
+    '12' : std*(2**11),
+
+}
 
 for row in sheet1.iter_rows():
     for cell in row:
-        if cell.value == 0:
-            cell.value = '<10'
-        elif cell.value == 1:
-            cell.value = '20'
-        elif cell.value == 2:
-            cell.value = '40'
-        elif cell.value == 3:
-            cell.value = '80'
-        elif cell.value == 4:
-            cell.value = '160'
-        elif cell.value == 5:
-            cell.value = '320'
-        elif cell.value == 6:
-            cell.value = '640'
-        elif cell.value == 7:
-            cell.value = '1280'
-        elif cell.value == 0.5:
-            cell.value = '10'
-        elif cell.value == 1.5:
-            cell.value = '30'
-        elif cell.value == 2.5:
-            cell.value = '60'
-        elif cell.value == 3.5:
-            cell.value = '120'
-        elif cell.value == 4.5:
-            cell.value = '240'
-        elif cell.value == 6.5:
-            cell.value = '480'
-        elif cell.value == 7.5:
-            cell.value = '960'
-        elif cell.value == 8.5:
-            cell.value = '1920'
+        value = str(cell.value)
+        if cell.value in convert:
+            cell.value = convert.get(str(cell.value), cell.value)
 
-excelFile.save('stefan-new-data.xlsx')
+
+name, blank = args.Excelfile.split('.xlsx')
+Hi.save('%s_py.xlsx' %name)
